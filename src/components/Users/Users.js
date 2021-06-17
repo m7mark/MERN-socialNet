6b020c0f6e93647e67eceadb4660eb4bf7d5e0c8
Mark Rodovsky <m@bible.com.ua>
2021-06-17 22:44:00 +0300

    add button 'Get Users'

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,42 +5,46 @@ import userIcon from './../../assets/userIcon.png'
 
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        // let headers = {'f1044d61-6ff5-426d-9719-a80a9bdbb47b': value}
-        // axios.post("https://social-network.samuraijs.com/api/1.0", {headers: headers});
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            debugger;
-            props.setUsers(response.data.items)
-        });
-    }
 
-    return <div> {
-        props.users.map(u =>
-            <div key={u.id}>
-                <span>
-                    <div>
-                        <img className={p.userimg} src={u.photos.small
-                            != null ? u.photos.small : userIcon} />
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => { props.unfollow(u.id) }}>Follow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>Unfollow</button>}
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div>{"u.location.country"}</div>
-                        <div>{"u.location.city"}</div>
-                    </span>
-                </span>
-            </div>
-        )
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            // let headers = {'f1044d61-6ff5-426d-9719-a80a9bdbb47b': value}
+            // axios.post("https://social-network.samuraijs.com/api/1.0", {headers: headers});
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                debugger;
+                props.setUsers(response.data.items)
+            });
+        }
     }
+    return <div>
+        <button onClick={getUsers}>Get Uers</button>
+        {
+            props.users.map(u =>
+                <div key={u.id}>
+                    <span>
+                        <div>
+                            <img className={p.userimg} src={u.photos.small
+                                != null ? u.photos.small : userIcon} />
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => { props.unfollow(u.id) }}>Follow</button>
+                                : <button onClick={() => { props.follow(u.id) }}>Unfollow</button>}
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </span>
+                    </span>
+                </div>
+            )
+        }
     </div>
 }
 
