976d8d8da8a9e914f7f9f578f076877804cde6dd
Mark Rodovsky <m@bible.com.ua>
2021-06-18 03:55:45 +0300

    55 pagination

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { followAC, setUsersAC, unfollowAC } from '../../redux/users-reducer';
+import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/users-reducer';
 import Users from './Users';
 
 let mapStateToProps = (state) => {
-    return { users: state.usersPage.users }
+    return {
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
+    }
 }
 let mapDispatchToProps = (dispatch) => {
     return {
         follow: (userId) => { dispatch(followAC(userId)) },
         unfollow: (userId) => { dispatch(unfollowAC(userId)) },
         setUsers: (users) => { dispatch(setUsersAC(users)) },
+        setCurrentPage: (pageNumber) => {dispatch(setCurrentPageAC(pageNumber)) },
+        setTotalUsersCount: (totalCount) => {dispatch(setTotalUsersCountAC(totalCount)) }
     }
 }
 
