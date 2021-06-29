45cd240c17894b3322daedc95cde863088673a77
Mark Rodovsky <m@bible.com.ua>
2021-06-29 23:28:38 +0300

    Add login form on login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,27 @@
 
-const Login = (props) => {
-return <div>
-    <h1>Login</h1>
-</div>
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input placeholder="Login" />
+            </div>
+            <div>
+                <input placeholder="Password" />
+            </div>
+            <div>
+                <input type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
 }
 
+const Login = (props) => {
+    return <>
+    <h1>Login</h1>
+    <LoginForm />
+    </>
+}
 export default Login
