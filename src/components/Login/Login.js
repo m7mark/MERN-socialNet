7a51c1b79ef5da5e954362a23a43af0fba9fa38c
Mark Rodovsky <m@bible.com.ua>
2021-07-03 02:48:48 +0300

    77 Login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,60 @@
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const LoginForm = (props) => {
+
+
+const LoginForm = ({ }) => {
+    const submit = (values, { resetForm }) => {
+        console.log(values)
+        resetForm({})
+    }
     return (
-        <form>
-            <div>
-                <input placeholder="Login" />
-            </div>
-            <div>
-                <input placeholder="Password" />
-            </div>
-            <div>
-                <input type={"checkbox"} /> remember me
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-        </form>
-    )
-}
+        <Formik
+            initialValues={{
+                email: '',
+                password: '',
+                rememberMe: false
+            }}
+            validationSchema={Yup.object({
+                email: Yup.string()
+                    .required('Required'),
+                password: Yup.string()
+                    .required('Required')
+            })}
+            onSubmit={submit}
+        >
+            <Form>
+                <Field
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    type="email"
+                />
+                <br />
+                <Field
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    type="password"
+                />
+                <br />
+                <label htmlfor="rememberMe">Remember me </label>
+                <Field
+                    id="rememberMe"
+                    name="rememberMe"
+                    type="checkbox"
+                />
+                <br />
+                <button type="submit">Send</button>
+            </Form>
+        </Formik>
+    );
+};
 
 const Login = (props) => {
     return <>
-    <h1>Login</h1>
-    <LoginForm />
+        <h1>Login</h1>
+        <LoginForm />
     </>
 }
 export default Login
