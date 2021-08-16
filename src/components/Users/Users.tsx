b61edc383b75b48907aa99836737f613e2753e1e
m7mark <m7mark@yandex.ru>
2021-08-16 21:43:39 +0300

    Update npm - package.json

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,11 +6,13 @@ import { UsersSearchForm } from './UsersSearchForm';
 import { FilterType, getUsers, follow, unfollow } from '../../redux/users-reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCurrentPage, getFollowingInProgress, getIsFetching, getPageSize, getTotalUsersCount, getUsersFilter, getUsersFromState } from '../../redux/users-selector';
+import { useHistory } from 'react-router-dom';
+
 
 type PropsType = {
     pageTitle: string
 }
-export const  Users: React.FC<PropsType> = (props) => {
+export const Users: React.FC<PropsType> = (props) => {
 
     const totalUsersCount = useSelector(getTotalUsersCount)
     const currentPage = useSelector(getCurrentPage)
@@ -19,16 +21,24 @@ export const  Users: React.FC<PropsType> = (props) => {
     const users = useSelector(getUsersFromState)
     const followingInProgress = useSelector(getFollowingInProgress)
     const isFetching = useSelector(getIsFetching)
+    const history = useHistory()
+    const queryString = require('query-string');
+
 
     useEffect(() => {
-        const filter: FilterType = {
-            term: '',
-            friend: null
-        }
-        setTimeout(() => {
-            dispatch(getUsers(1, pageSize, filter))
-        }, 100)
+        const parsed = queryString.parse(history.location.search);
+        console.log(parsed)
+        console.log(history.location.search)
+        dispatch(getUsers(currentPage, pageSize, filter))
     }, [])
+
+    // useEffect(() => {
+    //     history.push({
+    //         pathname: '/users',
+    //         search: `?term=${filter.term}&friend=${filter.friend}&page=${currentPage}`
+    //     })
+    // }, [filter, currentPage])
+
 
     const dispatch = useDispatch()
     const onPageChanged = (pageNumber: number) => {
