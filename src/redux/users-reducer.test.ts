8c3617d3e07dc6fc251b6b38e5994afa498c98f0
m7mark <m7mark@yandex.ru>
2021-08-21 20:10:18 +0300

    All tests pass

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -6,26 +6,30 @@ beforeEach(() => {
         users: [
             {
                 id: 0, name: 'Mark 0', followed: false,
-                photos: { small: null, large: null }, status: 'status 0'
+                photos: { small: undefined, large: undefined }, status: 'status 0'
             },
             {
                 id: 1, name: 'Mark 1', followed: false,
-                photos: { small: null, large: null }, status: 'status 1'
+                photos: { small: undefined, large: undefined }, status: 'status 1'
             },
             {
                 id: 2, name: 'Mark 2', followed: true,
-                photos: { small: null, large: null }, status: 'status 2'
+                photos: { small: undefined, large: undefined }, status: 'status 2'
             },
             {
                 id: 3, name: 'Mark 3', followed: true,
-                photos: { small: null, large: null }, status: 'status 3'
+                photos: { small: undefined, large: undefined }, status: 'status 3'
             }
         ],
         pageSize: 6,
         totalUsersCount: 0,
         currentPage: 1,
         isFetching: false,
-        followingInProgress: []
+        followingInProgress: [],
+        filter: {
+            term: '',
+            friend: null
+        }
     }
 })
 test('Follow success', () => {
