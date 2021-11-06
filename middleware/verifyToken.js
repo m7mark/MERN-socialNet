a51d01fea4a3193429cdf46cd825ca9661d4c589
m7mark <m7mark@yandex.ru>
2021-11-07 02:22:35 +0300

    return next()

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -6,14 +6,13 @@ const allAndVerifyToken = (req, res, next) => {
   if (req.method === 'OPTIONS') { next() }
   try {
     const authHeader = req.headers.authorization
-    if (!authHeader) { next() }
-    else {
-      const token = authHeader.split(' ')[1]
-      const decodeData = jwt.verify(token, process.env.JWT_SEC)
-      //include user data from token in to request
-      req.user = decodeData
-      next()
-    }
+    if (!authHeader) { return next() }
+    const token = authHeader.split(' ')[1]
+    if (token.length < 1 || token === 'null') { return next() }
+    const decodeData = jwt.verify(token, process.env.JWT_SEC)
+    //include user data from token in to request
+    req.user = decodeData
+    next()
   } catch {
     return next(createError(500, "Wrong Token"))
   }
@@ -24,6 +23,7 @@ const verifyToken = (req, res, next) => {
     const authHeader = req.headers.authorization
     if (!authHeader) { return next(createError(500, "Empty Token")) }
     const token = authHeader.split(' ')[1]
+    if (token.length < 1 || token === 'null') { return next(createError(500, "Empty Token")) }
     const decodeData = jwt.verify(token, process.env.JWT_SEC)
     //include user data from token in to request
     req.user = decodeData
@@ -35,9 +35,8 @@ const verifyToken = (req, res, next) => {
 
 const verifyTokenAndAdmin = (req, res, next) =>
   verifyToken(req, res, () => {
-    if (req.user?.roles.includes('ADMIN')) {
-      next()
-    } else { return next(createError(500, "No permittion")) }
+    if (req.user?.roles.includes('ADMIN')) { return next() }
+    else { return next(createError(500, "No permittion")) }
   })
 
 module.exports = { verifyToken, allAndVerifyToken, verifyTokenAndAdmin }
