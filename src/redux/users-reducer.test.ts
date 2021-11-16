ec50d37a451144fc47fe0e96781b09b6fce3efcd
m7mark <m7mark@yandex.ru>
2021-11-16 03:00:00 +0300

    Edit tests

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -5,19 +5,19 @@ beforeEach(() => {
     state = {
         users: [
             {
-                id: 0, name: 'Mark 0', followed: false,
+                id: '0', name: 'Mark 0', followed: false,
                 photos: { small: undefined, large: undefined }, status: 'status 0'
             },
             {
-                id: 1, name: 'Mark 1', followed: false,
+                id: '1', name: 'Mark 1', followed: false,
                 photos: { small: undefined, large: undefined }, status: 'status 1'
             },
             {
-                id: 2, name: 'Mark 2', followed: true,
+                id: '2', name: 'Mark 2', followed: true,
                 photos: { small: undefined, large: undefined }, status: 'status 2'
             },
             {
-                id: 3, name: 'Mark 3', followed: true,
+                id: '3', name: 'Mark 3', followed: true,
                 photos: { small: undefined, large: undefined }, status: 'status 3'
             }
         ],
@@ -34,13 +34,13 @@ beforeEach(() => {
 })
 test('Follow success', () => {
     // reducer()
-    const newState = usersReducer(state, actions.followSuccess(1))
+    const newState = usersReducer(state, actions.followSuccess('1'))
     expect(newState.users[0].followed).toBeFalsy()
     expect(newState.users[1].followed).toBeTruthy()
 })
 test('Unfollow success', () => {
     // reducer()
-    const newState = usersReducer(state, actions.unfollowSuccess(3))
+    const newState = usersReducer(state, actions.unfollowSuccess('3'))
     expect(newState.users[2].followed).toBeTruthy()
     expect(newState.users[3].followed).toBeFalsy()
 })
--- src/redux/users-reducer.thunk.test.ts
+++ src/redux/users-reducer.thunk.test.ts
@@ -1,6 +1,6 @@
 import { actions, follow, unfollow } from "./users-reducer"
 import { userAPI } from "../api/users-api"
-import { CommonResponseType, ResultCodeEnum } from "../api/api"
+import { CommonDataResponseType, ResultCodeEnum } from "../api/api"
 
 jest.mock('../api/users-api')
 const dispatchMock = jest.fn()
@@ -13,30 +13,32 @@ beforeEach(() => {
     userAPIMock.follow.mockClear()
 })
 
-const result: CommonResponseType = {
-    data: {},
-    messages: [],
-    resultCode: ResultCodeEnum.Success
+const result: CommonDataResponseType = {
+    data: {
+        data: {},
+        messages: [],
+        resultCode: ResultCodeEnum.Success
+    }
 }
 
 test('Follow thunk success', async () => {
     userAPIMock.follow.mockReturnValue(Promise.resolve(result))
-    const thunk = follow(3)
+    const thunk = follow('3')
 
     await thunk(dispatchMock, getState, {})
     expect(dispatchMock).toBeCalledTimes(3)
-    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 3))
-    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(3))
-    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, 3))
+    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, '3'))
+    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess('3'))
+    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, '3'))
 })
 
 test('Unfollow thunk success', async () => {
     userAPIMock.unfollow.mockReturnValue(Promise.resolve(result))
-    const thunk = unfollow(3)
+    const thunk = unfollow('3')
 
     await thunk(dispatchMock, getState, {})
     expect(dispatchMock).toBeCalledTimes(3)
-    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 3))
-    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.unfollowSuccess(3))
-    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, 3))
+    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, '3'))
+    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.unfollowSuccess('3'))
+    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, '3'))
 })
