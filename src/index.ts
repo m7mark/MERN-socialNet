738812065c8e9b1108fc22a486747a793ed55498
m7mark <m7mark@yandex.ru>
2021-11-22 00:45:33 +0300

    dirName

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,24 @@ import dotenv from "dotenv"
 import authRouter from "./routes/user"
 import profileRouter from "./routes/profile"
 import cors from "cors"
-import path, { dirname } from 'path';
+import path  from 'path';
 import { Request, Response, NextFunction } from 'express'
 
 // __dirname defenition
 // const __dirname = dirname(fileURLToPath(import.meta.url));
+const dirName = path.dirname(__dirname);
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "/uploads")))
-app.use(express.static(path.join(__dirname, "/loginpage/build")));
+app.use(express.static(path.join(dirName, "/uploads")))
+app.use(express.static(path.join(dirName, "/loginpage/build")));
 app.use('/api', authRouter)
 app.use('/api', profileRouter)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/loginpage/build', 'index.html'));
+  res.sendFile(path.join(dirName, '/loginpage/build', 'index.html'));
 });
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   res.status(200)
