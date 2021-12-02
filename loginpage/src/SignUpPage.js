13333581af1c45d8f9b18298d6db0cd9ca505c4c
m7mark <m7mark@yandex.ru>
2021-12-03 01:18:08 +0300

    Api documentation link

--- loginpage/src/SignUpPage.js
+++ loginpage/src/SignUpPage.js
@@ -113,6 +113,9 @@ export const SignUpPage = () => {
     <div className="signupWrapper">
       <div className="signupContainer">
         <SignUpForm />
+        <div>
+          <a href="https://documenter.getpostman.com/view/17550384/UV5f7Dvw" className="link">Open API documentation</a>
+        </div>
       </div>
     </div>
   </>
